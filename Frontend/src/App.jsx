f62af62afe17b7bf9f555215e1969ba48c45d2a1
Deepak Bharti <deepak17122002@gmail.com>
2025-07-25 21:23:21 +0530

    creating login and signup pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,17 +2,25 @@ import React from 'react'
 import Right from './home/right/Right.jsx'
 import Left from './home/left/Left.jsx'
 import Lougout from './home/left/Logout.jsx'
+import Signup from '../components/Signup.jsx'
+import Login from '../components/Login.jsx'
 
 function App() {
   return (
     <>
 
-      <div className='flex h-screen'>
+      {/* <div className='flex h-screen'>
 
-      <Lougout></Lougout>
+       <Lougout></Lougout>
         <Left></Left>
-        <Right></Right>
+        <Right></Right> 
+        
 
+      </div> */}
+
+      <div>
+        <Signup></Signup>
+         {/* <Login></Login> */}
       </div>
 
     </>
